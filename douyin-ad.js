c4d46eebd2f3536edcf1844faf19db87e80622e7
zhilun2021 <86742791+zhilun2021@users.noreply.github.com>
2021-10-04 15:03:20 +0800

    Update douyin-ad.js

--- douyin-ad.js
+++ douyin-ad.js
@@ -1,14 +1,17 @@
 #抖音去广告，适用于抖音16.2.0以下版本
 [mitm]
-hostname =  *.snssdk.com, *.amemv.com
+hostname =  aweme.snssdk.com, frontier.snssdk.com
 
 
 [rewrite_local]
 
-# 抖音去广告去水印 (By Choler)
-#URL Rewrite
-^https?+:\/\/[\w-]++\.amemv\.com\/aweme\/v\d\/feed\/ url request-header ^GET \/aweme\/v\d\/feed\/(.+)(\r\n)Host:.+ request-header GET /aweme/v1/feed/$1$2Host: aweme.snssdk.com
-^https?+:\/\/[\w-]++\.amemv\.com\/aweme\/v\d\/aweme\/post\/ url request-header ^GET \/aweme\/v\d\/aweme\/post\/(.+)(\r\n)Host:.+ request-header GET /aweme/v1/aweme/post/$1$2Host: aweme.snssdk.com
-^https?+:\/\/[\w-]++\.amemv\.com\/aweme\/v\d\/follow\/feed\/ url request-header ^GET \/aweme\/v\d\/follow\/feed\/(.+)(\r\n)Host:.+ request-header GET /aweme/v1/follow/feed/$1$2Host: aweme.snssdk.com
-^https?+:\/\/[\w-]++\.amemv\.com\/aweme\/v\d\/nearby\/feed\/ url request-header ^GET \/aweme\/v\d\/nearby\/feed\/(.+)(\r\n)Host:.+ request-header GET /aweme/v1/nearby/feed/$1$2Host: aweme.snssdk.com
-
+^https?:\/\/.+?\.(snssdk|amemv)\.com\/api\/ad\/ url reject-img
+^https?:\/\/.+?\.snssdk\.com\/motor\/operation\/activity\/display\/config\/V2\/ url reject
+^https?:\/\/[\s\S]*\.snssdk\.com\/api\/ad\/ url reject-img
+^https?:\/\/[\w-]+\.snssdk\.com\/.+_ad\/ url reject
+^https?:\/\/aweme\.snssdk\.com\/aweme\/v1\/aweme\/stats\/ url reject-img
+^https?:\/\/aweme\.snssdk\.com\/aweme\/v1\/device\/update\/ url reject-img
+^https?:\/\/aweme\.snssdk\.com\/aweme\/v1\/screen\/ad\/ url reject-img
+^https?:\/\/aweme\.snssdk\.com\/service\/1\/app_logout\/ url reject-img
+^https?:\/\/aweme\.snssdk\.com\/service\/2\/app_log url reject-img
+^https?:\/\/frontier\.snssdk\.com\/ url reject-img
