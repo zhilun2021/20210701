7b4171dd9f19ecfa2a2e143f82b0094b14909046
zhilun2021 <86742791+zhilun2021@users.noreply.github.com>
2021-10-04 15:09:45 +0800

    Update douyin-ad.js

--- douyin-ad.js
+++ douyin-ad.js
@@ -1,11 +1,12 @@
 #抖音去广告，适用于抖音16.2.0以下版本
 [mitm]
-hostname =  aweme.snssdk.com, frontier.snssdk.com
+hostname =  aweme.snssdk.com, frontier.snssdk.com, reading.snssdk.com, lark-frontier-hl.snssdk.com, *.snssdk.com, *.amemv.com
 
 
 [rewrite_local]
 
 ^https?:\/\/.+?\.(snssdk|amemv)\.com\/api\/ad\/ url reject-img
+^https?:\/\/[\w-]+\.(amemv|musical|snssdk|tiktokv)\.(com|ly)\/(api|motor)\/ad\/ url reject
 ^https?:\/\/.+?\.snssdk\.com\/motor\/operation\/activity\/display\/config\/V2\/ url reject
 ^https?:\/\/[\s\S]*\.snssdk\.com\/api\/ad\/ url reject-img
 ^https?:\/\/[\w-]+\.snssdk\.com\/.+_ad\/ url reject
@@ -15,3 +16,4 @@ hostname =  aweme.snssdk.com, frontier.snssdk.com
 ^https?:\/\/aweme\.snssdk\.com\/service\/1\/app_logout\/ url reject-img
 ^https?:\/\/aweme\.snssdk\.com\/service\/2\/app_log url reject-img
 ^https?:\/\/frontier\.snssdk\.com\/ url reject-img
+
